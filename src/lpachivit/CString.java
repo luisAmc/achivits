8d39119768ee7110abd147feee5a80617ca7ee00
Luis Martinez <luis_amc@unitec.edu>
2015-08-22 17:55:43 -0600

    CString

---
+++ src/lpachivit/CString.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lpachivit;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Luis Martinez
+ */
+public class CString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cadena a validar segun C:");
+        String email = sc.nextLine();
+        Pattern regex = Pattern.compile("^\"(\\w|\\d|\\s|\\\\\")*\"$");
+        Matcher matcher = regex.matcher(email);
+        if (matcher.find()) {
+            System.out.println("Cadena valido!");
+        } else {
+            System.out.println("Cadena NO valido!");
+        }
+    }
+}
